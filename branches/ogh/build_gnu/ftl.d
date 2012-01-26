3e8b540f8409c2b0fc1b99a300df7a0c27483e1c
wurikiji <wurikiji@2de97398-710c-2fb3-caca-c35a0f1fcdef>
2012-01-26 00:13:26 +0000

    a

--- branches/ogh/build_gnu/ftl.d
+++ branches/ogh/build_gnu/ftl.d
@@ -1,5 +1,5 @@
-ftl.o: ../ftl_tutorial_opt/ftl.c ../include/jasmine.h ../include/nand.h \
+ftl.o: ../ftl_dynamic_opt/ftl.c ../include/jasmine.h ../include/nand.h \
  ../include/flash.h ../include/sata.h ../include/sata_registers.h \
  ../include/sata_cmd.h ../include/mem_util.h ../target_spw/target.h \
  ../include/peri.h ../include/sdram.h ../include/bank.h \
- ../ftl_tutorial_opt/ftl.h ../target_spw/misc.h ../include/uart.h
+ ../ftl_dynamic_opt/ftl.h ../target_spw/misc.h ../include/uart.h
